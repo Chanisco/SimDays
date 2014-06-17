12ad6601f4b2b685175b6233664204c70d43897b
Chanisco <chaniscotromp@Chaniscos-MacBook-Pro.local>
2014-06-17 21:12:11 +0200

    Playercollision

---
+++ Assets/Script/Player/Collision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+	public int transport;
+
+	void Start(){
+		transport = Global.transport;
+	}
+
+	void OnCollisionEnter(){
+		if(transport > 10 && transport < 20){
+//			Global.
+		}
+	}
+}
