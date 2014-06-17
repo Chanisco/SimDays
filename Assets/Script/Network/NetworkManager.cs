278b62c1a2095022a7f27a437ac67a1fb1f6de59
Chanisco <chaniscotromp@chaniscos-mbp.media.local>
2014-06-17 14:37:55 +0200

    Error in Server
    I can’t continue this project at the time.
    The servers at school are protected so I can’t Start the server here

--- Assets/Script/Network/NetworkManager.cs
+++ Assets/Script/Network/NetworkManager.cs
@@ -7,42 +7,71 @@ public class NetworkManager : MonoBehaviour {
 	private const string gameName = "WalkRoom";
 
 	private HostData[] hostList;
-	private Rect buttonRect = new Rect(100,100,250,100);
+	private Rect startBtnRect = new Rect(100,100,250,100);
+	private Rect refreshBtnRect = new Rect(100,200,250,100);
+
+	private bool refreshing;
 
 	private void StartServer(){
-		Network.InitializeServer(4,1337,!Network.HavePublicAddress());
-		MasterServer.RegisterHost(typeName,gameName);
+		Network.InitializeServer(4,25000,!Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName,gameName,"Now lets run");
 	}
+
+	private void RefreshHostList(){
+		MasterServer.RequestHostList(typeName);
+		refreshing = true;
+	
+	}
+
 
 	void OnServerInitialized(){
 		Debug.Log("Server Initialized");
 	}
 
-	void OnGUI(){
-		if(!Network.isClient && !Network.isServer){
-			if(GUI.Button(buttonRect,"StartServer")){
-				StartServer();
-			}
-		}
 
-	}
-
-	private void RefreshHostList()
-	{
-		MasterServer.RequestHostList(typeName);
-	}
-	
 	void OnMasterServerEvent(MasterServerEvent msEvent)
 	{
-		if (msEvent == MasterServerEvent.HostListReceived)
-			hostList = MasterServer.PollHostList();
+		if (msEvent == MasterServerEvent.RegistrationSucceeded){
+			Debug.Log("Registerede Server");
+			//hostList = MasterServer.PollHostList();
+		}
 	}
-
+	
 	private void JoinServer(HostData hostdata){
 		Network.Connect(hostdata);
 	}
-
+	
 	void OnConnectedToServer(){
 		Debug.Log("Server Joined");
 	}
+
+
+	void Update(){
+		if(refreshing){
+			if(MasterServer.PollHostList().Length > 0){
+				refreshing = false;
+				Debug.Log(MasterServer.PollHostList().Length);
+			}
+		}
+	}
+	void OnGUI(){
+	//	if(!Network.isClient && !Network.isServer){
+			if(GUI.Button(startBtnRect,"StartServer")){
+				StartServer();
+			}
+			if(GUI.Button(refreshBtnRect,"Refresh")){
+				RefreshHostList();
+			}
+			/*if(hostList != null){
+				for(int i = 0; i < hostList.Length; i++){
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+						JoinServer(hostList[i]);
+				}
+			}*/
+	//	}
+
+	}
+
+
+
 }
