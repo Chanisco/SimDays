85ffad7c06da1126aa7f96dedae3746234c4c9e5
Chanisco <chaniscotromp@chaniscos-mbp.media.local>
2014-06-17 13:20:24 +0200

    Network Combining

---
+++ Assets/Script/Network/NetworkManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string typeName = "UniqueGameName";
+	private const string gameName = "WalkRoom";
+
+	private HostData[] hostList;
+	private Rect buttonRect = new Rect(100,100,250,100);
+
+	private void StartServer(){
+		Network.InitializeServer(4,1337,!Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName,gameName);
+	}
+
+	void OnServerInitialized(){
+		Debug.Log("Server Initialized");
+	}
+
+	void OnGUI(){
+		if(!Network.isClient && !Network.isServer){
+			if(GUI.Button(buttonRect,"StartServer")){
+				StartServer();
+			}
+		}
+
+	}
+
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+
+	private void JoinServer(HostData hostdata){
+		Network.Connect(hostdata);
+	}
+
+	void OnConnectedToServer(){
+		Debug.Log("Server Joined");
+	}
+}
